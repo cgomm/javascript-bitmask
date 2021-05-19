999dfef84cc688a817ab0a9ff582113f7c6e3ef1
Christian Gommans <c.gommans@outlook.de>
2021-05-19 16:39:46 +0200

    Added minimax

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,43 @@
+const helper = require('./helper-functions')
+
 const players = [0,1]
 const start = 0
+
+// TODO: test is needed
+const value = memoize(
+    (state, player) => {
+        if finished(state) {
+            return utility(state, player)
+        }
+        o = other(player)
+        nextStates = nextState(state, player);
+        result = [];
+        for (const ns in nextStates) {
+            result.push(-value(ns,o));
+        }
+        return Math.max(...result)
+    }
+)
+
+
+
+
+
+const bestMove = (state, player) => { 
+    nextStates = nextStates(state, player)
+    bestVal = value(state, player)
+    bestMoves = []
+    for(const s in nextStates) {
+        if(-value(s, other(player)) == bestVal) {
+            bestMoves.push(s)
+        }
+    }
+    bestState = choose(bestMoves)
+    return bestVal, bestState
+}
+
+console.log(factorial(5))
+console.log(factorial(6))
 
 // helper function: turn decimal into binary
 function dec2bin(dec) {
@@ -14,4 +52,4 @@ function set_bits(bits){
     return result
 }
 
-console.log(dec2bin(set_bits([1,3])));
+// console.log(dec2bin(set_bits([1,3])));
