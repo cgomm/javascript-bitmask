e0c53f8c8009241bcedcb43089656a3c7475424a
David <davod2211@gmail.com>
2021-05-21 13:41:14 +0200

    fixed play_game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,22 +1,102 @@
-// helper functions
-const choose = (choices) => {
-    var index = Math.floor(Math.random() * choices.length);
-    return choices[index]
+////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////// Tic-Tac-Toe-Bitboard Functions ///////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// helper function: remove set of elements from set
+function removeSetFromSet(originalSet, toBeRemovedSet) {
+    toBeRemovedSet.forEach(Set.prototype.delete, originalSet);
 }
 
-const memoize = (fn) => {
-    let cache  = {};
-    return (...args) => {
-        let n = args[0];
-        if (n in cache) {
-            return cache[n];
-        } else {
-            let result = fn(n);
-            cache[n] = result;
-            return result;
+function set_bits(bits){
+    let result = 0;
+    for (const b of bits){
+        result |= (1 << b);
+    }
+    return result;
+}
+
+function set_bit(n){
+    return 1 << n;
+}
+
+function empty(state){
+    let Free = new Set(Array(9).keys())
+
+    let new_arr = Array.from(Array(9).keys()).filter(n => ((state & (1 << n)) != 0));
+    removeSetFromSet(Free,new Set(new_arr));
+
+    new_arr = Array.from(Array(9).keys()).filter(n => ((state & (1 << (9+n))) != 0));
+    removeSetFromSet(Free,new Set(new_arr));
+
+    return Free;
+}
+
+function next_states(state, player){
+    let Empty = empty(state);
+    let Result = [];
+    for (n of Empty){
+        let next_state = state | set_bit(player * 9 + n);
+        Result.push(next_state);
+    }
+
+    return Result;
+}
+
+
+All_Lines = [ set_bits([0,1,2]), set_bits([3,4,5]), set_bits([6,7,8]),
+                set_bits([0,3,6]), set_bits([1,4,7]), set_bits([2,5,8]),
+                set_bits([0,4,8]), set_bits([2,4,6]) ]
+
+function utility(state, player){
+    for (mask of All_Lines){
+        if((state & mask) == mask){
+            return 1 - 2 * player;
+        }
+        if(((state >> 9)& mask)==mask){
+            return -1 +2 * player;
         }
     }
+    if (((state&511)|(state>>9))!=511){
+        return null;
+    }
+    return 0;
 }
+
+// s1 = set_bits([0, 2, 3, 6, 1+9,  4+9, 5+9])
+// console.log("u1: ",utility(s1,0));
+
+// s2 = set_bits([0, 2, 6, 8, 1+9, 4+9, 7+9])
+// console.log("u2: ",utility(s2,0));
+
+// s3 = set_bits([0, 2, 5, 6, 7, 1+9, 3+9, 4+9, 8+9])
+// console.log("u3: ",utility(s3,0));
+
+// s4 = set_bits([0, 2, 5, 6, 1+9, 3+9, 4+9]);
+// console.log("u4: ",utility(s4,0));
+
+function finished(state){
+    return utility(state,0)!=null;
+}
+
+
+function find_winner(state){
+    if (finished(state)){
+        if (utility(state,1)==1){
+            console.log("Player 0 won!!!!!!!")
+        }else if (utility(state,1)==-1){
+                console.log("Player 1 won!!!!!!!!")
+        }else{
+            console.log("untenschieden!!!!!!!")
+        }
+        return true
+    return false
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////// Output Functions /////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 const getDOMElements = () => {
     resultElements = [];
@@ -37,10 +117,11 @@ const editTextOfDOMElement = (e, text) => {
 }
 
 function printBoard(state){
-    x_state= state&511;
-    o_state= state>>9;
-    mask = 1;
-    for (cell of Array(9).keys()){
+    console.log("print")
+    let x_state= state&511;
+    let o_state= state>>9;
+    let mask = 1;
+    for (let cell of Array(9).keys()){
         if (x_state&mask){
             editTextOfDOMElement(boardFields[cell],"X");
         }
@@ -51,14 +132,56 @@ function printBoard(state){
     }
 }
 
-//test_state= set_bits([2,3,5,9+1,9+4,9+8])
 
 
-boardFields=getDOMElements();
-//printBoard(test_state)
+function get_move(state){
+    while (1){
+        let res= prompt("x,y of next move").split(",");
+        let row=parseInt(res[0]);
+        let col=parseInt(res[1]);
+        let mask = set_bit(9+row*3+col);
+        if ((state&mask)==0){
+            return state|mask;
+        }else{
+            console.log("wrong input");
+        }
+    }
+}
 
 
+// helper function: turn decimal into binary
+function dec2bin(dec) {
+    return (dec >> 0).toString(2);
+}
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////// Minimax Functions /////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// helper function: choose random element
+const choose = (choices) => {
+    var index = Math.floor(Math.random() * choices.length);
+    return choices[index]
+}
+
+
+let cache  = {};
+
+const memoize = (fn) => {
+    return (...args) => {
+        let arg1 = args[0];
+        let arg2 = args[1];
+        if ([arg1,arg2] in cache) {
+            //console.log("using mem func");
+            return cache[[arg1,arg2]];
+        } else {
+            let result = fn(arg1,arg2);
+            cache[[arg1,arg2]] = result;
+            return result;
+        }
+    }
+}
 
 function other(p){
     if(p==0){
@@ -68,156 +191,96 @@ function other(p){
 }
 
 
-// main programm
-const players = [0,1]
-const start = 0
+let num_value_calls=0;
 
 // TODO: test is needed
 const value = memoize(
     (state, player) => {
+        //console.log("value(",state,",",player,")")
+        num_value_calls+=1;
         if (finished(state)) {
+            //console.log("util(",state,",",player,"): ",utility(state, player))
             return utility(state, player)
         }
-        o = other(player)
-        nextStates = next_states(state, player);
-        result = [];
-        for (const ns in nextStates) {
-            result.push(-value(ns,o));
+        let other_player = other(player)
+        let ns = next_states(state, player);
+        //console.log("ns: ",ns)
+        let result = [];
+        for (const s of ns) {
+            //console.log(s)
+            //console.log("player: ",player, " other: ",other_player);
+            let new_val= value(s,other_player);
+            //console.log(new_val)
+            result.push(-new_val);
         }
+        //console.log("result: ",result);
         return Math.max(...result)
     }
 )
 
-const bestMove = (state, player) => { 
-    console.log("bestmove");
-    nextStates = next_states(state, player)
-    console.log("got next states");
-    bestVal = value(state, player)
-    console.log("got values");
-    bestMoves = []
-    console.log("1");
-    for(const s in nextStates) {
-        if(-value(s, other(player)) == bestVal) {
+//console.log("result value: ",value(set_bits([0, 6, 2+9]),1));
+
+const best_move = (state, player) => { 
+    //console.log("bestmove");
+    let ns = next_states(state, player)
+    //console.log("got next states: ",ns);
+    //console.log("value(",state,",",player,")")
+    let bestVal = value(state, player)
+    //console.log("got bestVal: ",bestVal);
+    let bestMoves = []
+    for(const s of ns) {
+        let other_val= value(s, other(player));
+        console.log("other val of:", s);
+        console.log("other_val: ",other_val);
+        if(-other_val == bestVal) {
             bestMoves.push(s)
         }
     }
-    
+    //console.log("best_moves:");
+    //console.log(bestMoves);
     bestState = choose(bestMoves)
     return [bestVal, bestState]
 }
 
-// helper function: turn decimal into binary
-function dec2bin(dec) {
-    return (dec >> 0).toString(2);
-}
-
-// helper function: remove set of elements from set
-function removeSetFromSet(originalSet, toBeRemovedSet) {
-    toBeRemovedSet.forEach(Set.prototype.delete, originalSet);
-}
-
-function set_bits(bits){
-    let result = 0;
-    for (const b of bits){
-        result |= (1 << b);
-    }
-    return result;
-}
-
-function set_bit(n){
-    return 1 << n;
-}
-
-function empty(state){
-    Free = new Set(Array(9).keys())
-
-    let new_arr = Array.from(Array(9).keys()).filter(n => ((state & (1 << n)) != 0));
-    removeSetFromSet(Free,new Set(new_arr));
-
-    new_arr = Array.from(Array(9).keys()).filter(n => ((state & (1 << (9+n))) != 0));
-    removeSetFromSet(Free,new Set(new_arr));
-
-    return Free;
-}
-
-function next_states(state, player){
-    Empty = empty(state);
-    Result = [];
-    for (n of Empty){
-        next_state = state | set_bit(player * 9 + n);
-        Result.push(next_state);
-    }
-    return Result;
-}
-
-All_Lines = [ set_bits([0,1,2]), set_bits([3,4,5]), set_bits([6,7,8]),
-                set_bits([0,3,6]), set_bits([1,4,7]), set_bits([2,5,8]),
-                set_bits([0,4,8]), set_bits([2,4,6]) ]
-
-function utility(state, player){
-    for (mask of All_Lines){
-        if((state & mask) == mask){
-            return 1 - 2 * player;
-        }
-        if(((state >> 9)& mask)==mask){
-            return -1 +2 * player;
-        }
-    }
-    if ((state&511)|(state>>9)!=511){
-        return null;
-    }
-    return 0;
-}
-
-function finished(state){
-    return utility(state,0)!=null;
-}
-
-
-//console.log(dec2bin(set_bits([1,3])));
-//console.log(empty(9))
-//console.log(next_states(1,1));
-//console.log(All_Lines)
-//console.log(finished(7))
-
+//console.log(best_move(0,0));
 
 function play_game(){
     state = start;
     while (1){
-        console.log("iteration");
         firstPlayer = players[0];
-        res= bestMove(state, firstPlayer);
+        //console.log("calc best move");
+        res= best_move(state, firstPlayer);
+        //console.log("got best move");
         val=res[0];
         state =res[1];
-        console.log("Value:",val);
+        //console.log("Value:",val);
+        //console.log("state: ",state)
         printBoard(state);
         if (finished(state)){
-            console.log("player 0 finished");
+            find_winner(state);
             return;
         } 
-        state= get_input(state);
+        res= best_move(state, other(firstPlayer));
+        //console.log("got best move");
+        val=res[0];
+        state =res[1];
+        //state= get_move(state);
         printBoard(state);
         if (finished(state)){
-            console.log("player 1 finished");
+            find_winner(state);
             return;
         }
     }
 }
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// Main Program /////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
 
-function get_input(state){
-    while (1){
-        res= prompt("x,y of next move").split(",");
-        row=res[0];
-        col=res[1];
-        mask = set_bit(9+row*3+col);
-        if ((state&mask)==0){
-            return state|mask;
-        }else{
-            console.log("wrong input");
-        }
-    }
-}
+const players = [0,1]
+const start = 0
 
-play_game();
+boardFields=getDOMElements();
+
+//play_game();
